b4c37fe9e63bcf73ca648670e6da92b3e46b55b6
Tyler Gener <tylergen33@gmail.com>
2023-06-16 11:55:25 -0700

    removed google auth

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import reducers from "./reducers";
 
 import App from "./App";
 import "./index.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
